b10e7452aaaec5147be696db6351bafa5845b9a3
Victor Krogh Hansen <kingodk@outlook.dk>
2023-04-12 17:25:04 +0200

    Added Unit Of Work ContainerBuilder Extensions method for registering UnitOfWork

---
+++ VictorKrogh.NET.Data.Extensions.Autofac.DependencyInjection/UnitOfWorkContainerBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using System.Data;
+
+namespace VictorKrogh.NET.Data.Extensions.Autofac.DependencyInjection;
+
+public static class UnitOfWorkContainerBuilderExtensions
+{
+    public static ContainerBuilder RegisterUnitOfWork(this ContainerBuilder containerBuilder)
+    {
+        containerBuilder
+            .RegisterType<UnitOfWorkFactory>()
+            .As<IUnitOfWorkFactory>()
+            .InstancePerLifetimeScope();
+
+        containerBuilder.Register((cc, p) =>
+        {
+            var unitOfWorkFactory = cc.Resolve<IUnitOfWorkFactory>();
+
+            if (p.Any())
+            {
+                return unitOfWorkFactory.Create(p.TypedAs<IsolationLevel>());
+            }
+
+            return unitOfWorkFactory.Create();
+        })
+            .AsImplementedInterfaces()
+            .InstancePerLifetimeScope();
+
+        return containerBuilder;
+    }
+}
