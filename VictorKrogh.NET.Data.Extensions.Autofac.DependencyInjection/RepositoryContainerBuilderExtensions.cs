ecf596ccc956d6bc65334d1a97152788611d2663
Victor Krogh Hansen <kingodk@outlook.dk>
2023-04-12 17:21:05 +0200

    Added Repository ContainerBuilder Extentions method for registering repositories for a given Provider

---
+++ VictorKrogh.NET.Data.Extensions.Autofac.DependencyInjection/RepositoryContainerBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System.Reflection;
+using VictorKrogh.NET.Data.Provider;
+using VictorKrogh.NET.Data.Repository;
+
+namespace VictorKrogh.NET.Data.Extensions.Autofac.DependencyInjection;
+
+public static class RepositoryContainerBuilderExtensions
+{
+    public static ContainerBuilder RegisterRepositories<TProvider>(this ContainerBuilder containerBuilder)
+        where TProvider : class, IProvider
+    {
+        containerBuilder.RegisterRepositories(typeof(TProvider).Assembly);
+
+        return containerBuilder;
+    }
+
+    public static ContainerBuilder RegisterRepositories(this ContainerBuilder containerBuilder, Assembly assembly)
+    {
+        containerBuilder.RegisterAssemblyTypes(assembly)
+            .AssignableTo<IRepository>()
+            .AsImplementedInterfaces();
+
+        return containerBuilder;
+    }
+}
